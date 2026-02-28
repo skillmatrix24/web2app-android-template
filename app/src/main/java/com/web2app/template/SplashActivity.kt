d152a4afd3e9ecd0727922a65258f1dbe7ef24f1
Khoman Sahu <khomans@Khomans-MacBook-Air.local>
2026-02-28 17:13:45 +0530

    Add splash activity and layout (stable version)

---
+++ app/src/main/java/com/web2app/template/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.web2app.template
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SPLASH_DELAY = 3000
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_DELAY)
+    }
+}
